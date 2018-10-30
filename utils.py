ecea34918b2000663bda2efdec02396749d05f4d
Michal <michal.tyszkiewicz@gmail.com>
2018-10-30 23:51:27 +0100

    Working on ellipsis in dimcheck

--- utils.py
+++ utils.py
@@ -73,12 +73,37 @@ class ShapeDict(dict):
     def update(self, other):
         for key in other.keys():
             if key in self and not self[key] == other[key]:
-                raise ShapeError("key already found with a different value") 
+                raise LabeledShapeError(key, self[key], other[key])
             else:
                 self[key] = other[key]
                 
 class ShapeError(Exception):
     pass
+
+class SizeMismatch(ShapeError):
+    def __init__(self, dim, expected, found):
+        self.dim = dim
+        self.expected = expected
+        self.found = found
+        self.param_name = None
+
+    def __str__(self):
+        fmt = "Size mismatch on dimension {} of argument `{}` (found {}, expected {})"
+        msg = fmt.format(self.dim, self.param_name, self.found, self.expected)
+        return msg
+
+class LabeledShapeError(ShapeError):
+    def __init__(self, label, prev_val, new_val):
+        self.label = label
+        self.prev_val = prev_val
+        self.new_val = new_val
+        self.param_name = None
+
+    def __str__(self):
+        fmt = ("Label `{}` already had dimension {} bound to it, "
+               "but it appears with dimension {} in tensor {}")
+        msg = fmt.format(self.label, self.prev_val, self.new_val, self.param_name)
+        return msg
 
 def get_bindings(tensor, annotation):
     n_ellipsis = annotation.count(...)
@@ -94,44 +119,38 @@ def get_bindings(tensor, annotation):
 
     bindings = ShapeDict()
     # check if dimensions match, one by one
-    for dim, anno in zip(tensor.shape, annotation):
+    for i, (dim, anno) in enumerate(zip(tensor.shape, annotation)):
         if isinstance(anno, str):
             # named wildcard, add to dict
             bindings.update({anno: dim})
+        elif anno == ...:
+            # ellipsis - done checking from the front, skip to checking in reverse
+            break
         elif isinstance(anno, int) and anno != dim:
             if anno == -1:
                 # anonymous wildcard dimension, continue
                 continue
-            elif anno == ...:
-                # ellipsis - done checking from the front, skip to checking in reverse
-                break
             else:
-                # mismatch
-                fmt = "Annotation requires dimension {}, got {}"
-                msg = fmt.format(anno, dim)
-                raise ShapeError(msg)
+                raise SizeMismatch(i, anno, dim)
 
     if n_ellipsis == 0:
         # no ellipsis - we don't have to go in reverse
         return bindings
 
     # there was an ellipsis, we have to check in reverse
-    for dim, anno in zip(tensor.shape[::-1], anonotation[::-1]):
+    for i, (dim, anno) in enumerate(zip(tensor.shape[::-1], annotation[::-1])):
         if isinstance(anno, str):
             # named wildcard, add to dict
             bindings.update({anno: dim})
+        elif anno == ...:
+            # ellipsis - done checking from the back, return
+            return bindings
         elif isinstance(anno, int) and anno != dim:
             if anno == -1:
                 # anonymous wildcard dimension, continue
                 continue
-            elif anno == ...:
-                # ellipsis - done checking from the back, return
-                return bindings
             else:
-                # mismatch
-                fmt = "Annotation requires dimension {}, got {}"
-                msg = fmt.format(anno, dim)
-                raise ShapeError(msg)
+                raise SizeMismatch(len(annotation) - i, anno, dim)
 
     raise AssertionError("Arrived at the end of procedure")
 
@@ -150,14 +169,22 @@ def shape_checked(func):
         for i, arg in enumerate(args):
             if i in checked_parameters:
                 param = checked_parameters[i]
-                shapes = get_bindings(arg, param.annotation)
-                shape_bindings.update(shapes)
+                try:
+                    shapes = get_bindings(arg, param.annotation)
+                    shape_bindings.update(shapes)
+                except ShapeError as e:
+                    e.param_name = param.name
+                    raise
 
         result = func(*args, **kwargs)
 
         if isinstance(sig.return_annotation, list):
-            shapes = get_bindings(result, sig.return_annotation)
-            shape_bindings.update(shapes)
+            try:
+                shapes = get_bindings(result, sig.return_annotation)
+                shape_bindings.update(shapes)
+            except ShapeError as e:
+                e.param_name = '<return value>'
+                raise
 
         return result
 
@@ -192,8 +219,10 @@ if __name__ == '__main__':
             t1 = torch.randn(3, 5)
             t2 = torch.randn(5, 3)
 
-            with self.assertRaises(ShapeError):
+            msg = "Size mismatch on dimension 1 of argument `t1` (found 5, expected 3)"
+            with self.assertRaises(ShapeError) as ex:
                 shape_checked(f)(t1, t2)
+            self.assertEqual(str(ex.exception), msg)
 
         def test_fails_wrong_return(self):
             def f(t1: [3, 5], t2: [5, 3]) -> [5]:
@@ -202,8 +231,11 @@ if __name__ == '__main__':
             t1 = torch.randn(3, 5)
             t2 = torch.randn(5, 3)
 
-            with self.assertRaises(ShapeError):
+            msg = ("Size mismatch on dimension 0 of argument "
+                   "`<return value>` (found 3, expected 5)")
+            with self.assertRaises(ShapeError) as ex:
                 shape_checked(f)(t1, t2)
+            self.assertEqual(str(ex.exception), msg)
 
         def test_fails_parameter_label_mismatch(self):
             def f(t1: [3, 'a'], t2: ['a', 3]) -> [3]:
@@ -224,5 +256,14 @@ if __name__ == '__main__':
 
             with self.assertRaises(ShapeError):
                 shape_checked(f)(t1, t2)
+
+        def test_succeeds_ellipsis(self):
+            def f(t1: [5, ..., 'a'], t2: ['a', ..., 5]) -> ['a']:
+                return (t1.transpose(0, 3) * t2).sum(dim=(1, 2, 3))
+                 
+            t1 = torch.randn(5, 1, 2, 3)
+            t2 = torch.randn(3, 1, 2, 5)
+
+            self.assertTrue((f(t1, t2) == shape_checked(f)(t1, t2)).all())
 
     unittest.main(failfast=True)
