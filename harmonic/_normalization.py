Add tests for batch/instance normalization; fix bugs in _normalization.py

harmonic/_normalization.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 from torch_localize import localized_module
 
-from .cmplx import magnitude
+from .cmplx import magnitude, magnitude_sqr
 
 class _Normalization(nn.Module):
     def __init__(self, repr, dim=2, eps=1e-2, kind='batch'):
@@ -35,20 +35,21 @@ class _Normalization(nn.Module):
         elif self._kind == 'instance':
             batch_dim = x.shape[1]
 
+        # transpose such that we can reduce over batches (in case of batch
+        # norm) while keeping feature channels separate, then flatten
+        # the spatial + (in case of batch norm) batch dimensions
         flat = x.transpose(1, 2).reshape(2, self.n_features * batch_dim, -1)
 
-        # compute mean
+        # compute mean and std
         means = flat.mean(dim=2, keepdim=True)
+        mean_corrected = flat - means
 
-        # compute std
-        stds = magnitude(flat - means).std(dim=1) + self.eps
+        stds = magnitude(mean_corrected).std(dim=1, keepdim=True)
+        std_corrected = mean_corrected / stds.unsqueeze(0)
 
-        spatial = [1] * self._dim
-
-        mean_corrected = x - means.reshape(2, batch_dim, -1, *spatial)
-        std_corrected = mean_corrected / stds.reshape(1, batch_dim, -1, *spatial)
-
-        return std_corrected
+        # recover the transposed shape then transpose back into the original shape
+        transposed_shape = (2, x.shape[2], x.shape[1], *x.shape[3:])
+        return std_corrected.reshape(*transposed_shape).transpose(1, 2)
 
     def __repr__(self):
         fmt = '{}Norm{}d(repr={}, eps={})'

harmonic/cmplx.py
@@ -2,12 +2,19 @@ import torch
 from torch_dimcheck import dimchecked
 
 @dimchecked
+def magnitude_sqr(t: [2, ...]) -> [...]:
+    return t.pow(2).sum(dim=0)
+
+
+@dimchecked
 def magnitude(t: [2, ...], eps=1e-8) -> [...]:
-    return torch.sqrt(t.pow(2).sum(dim=0) + eps)
+    return torch.sqrt(magnitude_sqr(t) + eps)
+
 
 @dimchecked
 def cmplx(real: [...], imag: [...]) -> [2, ...]:
     return torch.stack([real, imag], dim=0)
+
 
 @dimchecked
 def from_real(real: [...]) -> [2, ...]:

tests/d2/inorm_tests.py
@@ -1,21 +1,52 @@
 import torch, unittest
 from harmonic.d2 import InstanceNorm2d
+from harmonic.cmplx import magnitude
 from utils import rot90
 
 class INormTests(unittest.TestCase):
+    def test_featurewise_normalization(self):
+        b, h, w = 5, 30, 40
+
+        repr = (2, 3)
+        inorm = InstanceNorm2d(repr)
+
+        inp = torch.randn(2, b, sum(repr), h, w)
+        feature_means = torch.randn(5).reshape(1, 1, -1, 1, 1)
+        batch_means = torch.randn(b).reshape(1, -1, 1, 1, 1)
+        feature_stds = torch.randn(5).reshape(1, 1, -1, 1, 1)
+        batch_stds = torch.randn(b).reshape(1, -1, 1, 1, 1)
+        inp *= feature_stds
+        inp *= batch_stds
+        inp += feature_means
+        inp += batch_means
+
+        out = inorm(inp)
+        mags = magnitude(out)
+
+        for batch_item in range(b):
+            for feature in range(sum(repr)):
+                mag = mags[batch_item, feature, ...]
+                val = out[:, batch_item, feature, ...]
+
+                diff_std = torch.abs(mag.std() - 1.)
+                diff_mean = torch.abs(val.mean())
+                self.assertLess(diff_mean.item(), 0.1)
+                self.assertLess(diff_std.item(), 0.1)
+
+
     def test_equivariance_eval(self):
         b, h, w = 5, 30, 30
 
         repr = (2, 3)
-        bnorm = InstanceNorm2d(repr).double()
+        inorm = InstanceNorm2d(repr).double()
 
         inp = torch.randn(2, b, sum(repr), h, w, dtype=torch.float64)
         rot = rot90(inp)
 
         # eval mode
-        bnorm.eval()
-        base_fwd = bnorm(inp)
-        rot_fwd = bnorm(rot)
+        inorm.eval()
+        base_fwd = inorm(inp)
+        rot_fwd = inorm(rot)
 
         diff = (rot90(base_fwd) - rot_fwd).max().item()
         self.assertLess(diff, 1e-5)
@@ -23,15 +54,15 @@ class INormTests(unittest.TestCase):
     def test_equivariance_train(self):
         b, h, w = 5, 30, 30
         repr = (2, 3)
-        bnorm = InstanceNorm2d(repr).double()
+        inorm = InstanceNorm2d(repr).double()
 
         inp = torch.randn(2, b, sum(repr), h, w, dtype=torch.float64)
         rot = rot90(inp)
 
         # train mode
-        bnorm.train()
-        base_fwd = bnorm(inp)
-        rot_fwd = bnorm(rot)
+        inorm.train()
+        base_fwd = inorm(inp)
+        rot_fwd = inorm(rot)
 
         diff = (rot90(base_fwd) - rot_fwd).max().item()
         self.assertLess(diff, 1e-5)
@@ -39,15 +70,15 @@ class INormTests(unittest.TestCase):
     def test_backward_jit(self):
         b, h, w = 5, 30, 30
         repr = (2, 3)
-        bnorm = InstanceNorm2d(repr).double()
+        inorm = InstanceNorm2d(repr).double()
 
         inp = torch.randn(2, b, sum(repr), h, w, dtype=torch.float64)
         rot = rot90(inp)
 
         # train mode
-        bnorm.train()
-        base_fwd = bnorm(inp)
-        rot_fwd = bnorm(rot)
+        inorm.train()
+        base_fwd = inorm(inp)
+        rot_fwd = inorm(rot)
 
         diff = (rot90(base_fwd) - rot_fwd).max().item()
         self.assertLess(diff, 1e-5)