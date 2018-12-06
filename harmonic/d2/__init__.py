9a7200f4805958109bc00ba63b94d60313efaf8b
Michal <michal.tyszkiewicz@gmail.com>
2018-12-06 16:11:55 +0100

    Add ItemNorm2d to __init__.py

--- harmonic/d2/__init__.py
+++ harmonic/d2/__init__.py
@@ -1,5 +1,6 @@
 from .bnorm import BatchNorm2d
 from .inorm import InstanceNorm2d
+from .itnorm import ItemNorm2d
 from .conv import HConv2d, HConv2dTranspose, HConv1x1_2d
 from .nonl import ScalarGate2d
 from .pooling import avg_pool2d, upsample_2d
