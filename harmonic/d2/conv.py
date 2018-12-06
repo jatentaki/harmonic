d85cf2a41f0c6e0e4e438a50a804f4112dfca53e
Michal <michal.tyszkiewicz@gmail.com>
2018-12-06 10:37:03 +0100

    Bugfixes

--- harmonic/d2/conv.py
+++ harmonic/d2/conv.py
@@ -16,14 +16,14 @@ class HConv2d(_HConv):
 
 class HConv2dTranspose(_HConv):
     def __init__(self, in_repr, out_repr, size, radius=None, conv_kwargs=dict()):
-        super(HConv2d, self).__init__(
+        super(HConv2dTranspose, self).__init__(
             in_repr, out_repr, size, dim=2, radius=radius,
             transpose=True, conv_kwargs=conv_kwargs
         )
 
     @dimchecked
     def forward(self, x: [2, 'b', 'fi', 'hx', 'wx']) -> [2, 'b', 'fo', 'ho', 'wo']:
-        return super(HConv2d, self).forward(x)
+        return super(HConv2dTranspose, self).forward(x)
 
 class HConv1x1_2d(_HConv_1x1):
     def __init__(self, in_repr, out_repr):
