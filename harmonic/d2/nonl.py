a9b28cd1f77e2571f75df0d0d3c9ec3c8eeb40a5
Michal <michal.tyszkiewicz@gmail.com>
2018-12-08 16:01:54 +0100

    Make default scalar gate multiplicity 1

--- harmonic/d2/nonl.py
+++ harmonic/d2/nonl.py
@@ -1,7 +1,7 @@
 from .._scalar_gate import _ScalarGate
 
 class ScalarGate2d(_ScalarGate):
-    def __init__(self, repr, mult=2):
+    def __init__(self, repr, mult=1):
         super(ScalarGate2d, self).__init__(repr, dim=2, mult=mult)
 
     def forward(self, x: [2, 'b', 'f', 'h', 'w']) -> [2, 'b', 'f', 'h', 'w']:
