0ecaee87f51f1cea09898d76164ba81eb7bd7c25
Michal <michal.tyszkiewicz@gmail.com>
2018-12-10 17:12:11 +0100

    Dropout implemented, untested

--- harmonic/d2/__init__.py
+++ harmonic/d2/__init__.py
@@ -5,3 +5,4 @@ from .conv import HConv2d, HConv2dTranspose, HConv1x1_2d
 from .nonl import ScalarGate2d
 from .pooling import avg_pool2d, upsample_2d
 from .cat import cat2d
+from .dropout import Dropout2d
---
+++ harmonic/d2/dropout.py
@@ -0,0 +1,36 @@
+import torch
+
+class Dropout2d(torch.jit.ScriptModule):
+    __constants__ = ['p']
+
+    def __init__(self, p=0.1):
+        if not 0. < p < 1.:
+            raise ValueError(f"`p` must be between 0. and 1., got {p}")
+
+        super(Dropout2d, self).__init__()
+        self.p = p
+
+    @torch.jit.script_method
+    def forward(self, x):
+        if self.training:
+            sample = torch.rand(
+                (1, 1, x.size(2), 1, 1),
+                dtype=torch.float32, device=x.device
+            )
+
+            should_drop = (sample < self.p).to(torch.float32)
+            result = x * should_drop / (1. - self.p)
+        else:
+            result = x
+
+        return result
+
+    def __repr__(self):
+        return f'Dropout2d(p={self.p})'
+
+if __name__ == '__main__':
+    data = torch.randn(2, 1, 5, 2, 2) 
+    d = Dropout2d(p=0.6)
+    o = d(data)
+    print(o)
+    print(d)
